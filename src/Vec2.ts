58ab53c4d055b381925535eb17c3a5184aab9d1a
Michael Khryashchev <me@xiberfix.dev>
2022-05-08 00:29:21 +0400

    Add Vec2 type

---
+++ src/Vec2.ts
@@ -0,0 +1,24 @@
+export class Vec2 {
+    constructor(
+        public x: number,
+        public y: number,
+    ) {}
+
+    add(rhs: Vec2): Vec2 { return new Vec2(this.x + rhs.x, this.y + rhs.y) }
+    sub(rhs: Vec2): Vec2 { return new Vec2(this.x - rhs.x, this.y - rhs.y) }
+    mul(rhs: Vec2): Vec2 { return new Vec2(this.x * rhs.x, this.y * rhs.y) }
+    div(rhs: Vec2): Vec2 { return new Vec2(this.x / rhs.x, this.y / rhs.y) }
+
+    scale(s: number): Vec2 { return new Vec2(this.x * s, this.y * s) }
+
+    dot(rhs: Vec2): number { return this.x * rhs.x + this.y * rhs.y }
+
+    get lengthSq(): number { return this.dot(this) }
+    get length(): number { return Math.sqrt(this.lengthSq) }
+
+    distanceToSq(v: Vec2): number { return this.sub(v).lengthSq }
+    distanceTo(v: Vec2): number { return this.sub(v).length }
+
+    min(rhs: Vec2): Vec2 { return new Vec2(Math.min(this.x, rhs.x), Math.min(this.y, rhs.y)) }
+    max(rhs: Vec2): Vec2 { return new Vec2(Math.max(this.x, rhs.x), Math.max(this.y, rhs.y)) }
+}
