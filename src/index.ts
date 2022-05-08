0792f3a4da2186129cf3ca838df9a488fe3f002e
Michael Khryashchev <me@xiberfix.dev>
2022-05-08 22:08:02 +0400

    Add drawing surface

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,17 @@
 import './style.css'
 
-console.log('Hello!')
+
+// Prepare drawing surface
+
+const canvas = document.getElementById('surface') as HTMLCanvasElement
+const context = canvas.getContext('2d')
+
+function scaleCanvas(canvas: HTMLCanvasElement) {
+    const {width, height} = canvas.getBoundingClientRect()
+    const dpr = devicePixelRatio
+    canvas.width = Math.floor(width * dpr)
+    canvas.height = Math.floor(height * dpr)
+    context.scale(dpr, dpr)
+}
+
+scaleCanvas(canvas)
